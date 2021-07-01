25e72cb438f34e358f2d21f13ca27b7b5b58571a
wenfeng9321 <67744179+wenfeng9321@users.noreply.github.com>
2021-07-01 17:24:27 +0800

    Add files via upload

--- sept-task1-wenfeng9321-master/src/cn/edu/whut/sept/zuul/Parser.java
+++ sept-task1-wenfeng9321-master/src/cn/edu/whut/sept/zuul/Parser.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 *这个类是“Zuul世界”应用程序的一部分。
 *“Zuul的世界”是一个非常简单的，基于文本的冒险游戏。
 *
-*此解析器读取用户输入并尝试将其解释为“冒险”
-*命令。每次调用它时，它都从终端读取一条线路
-*尝试将该行解释为两个单词的命令。它返回命令
-*作为类命令的对象。
-*
-*解析器有一组已知的命令字。它检查用户的输入
-*已知命令，如果输入不是已知命令之一，则
-*返回标记为未知命令的命令对象。
-*
-*@作者迈克尔·Kö林和大卫J。巴恩斯
-*@版本2016.02.29
+*此解析器读取用户输入并尝试将其解释为“冒险”命令。
+*每次调用它时，它都从终端读取一条线路尝试将该行解释为两个单词的命令。
+*它返回命令作为类命令的对象。
+*解析器有一组已知的命令字。
+*它检查用户的输入已知命令，如果输入不是已知命令之一，则返回标记为未知命令的命令对象。
+* @author  Michael Kölling and David J. Barnes
+* @version 1.0
 */
+
 public class Parser
 {
     private CommandWords commands;  // holds all valid command words
@@ -33,31 +30,29 @@ public class Parser
     }
 
     /**
-     * @return The next command from the user.
      * 从用户返回下一个命令。
      */
     public Command getCommand()
     {
-        String inputLine;   // will hold the full input line      将保留完整的输入行
+        String inputLine;   // 将保留完整的输入行
         String word1 = null;
         String word2 = null;
 
-        System.out.print("> ");     // print prompt   
+        System.out.print("> ");     // 输入提示  
 
         inputLine = reader.nextLine();
 
-        // Find up to two words on the line.
+        // 查找输入的最多两个单词
         Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
-            word1 = tokenizer.next();      // get first word
+            word1 = tokenizer.next();      // 得到输入的第一个单词
             if(tokenizer.hasNext()) {
-                word2 = tokenizer.next();      // get second word
-                // note: we just ignore the rest of the input line.  注意：我们只忽略输入行的其余部分。
+                word2 = tokenizer.next();      // 得到输入的第二个单词
+                // 注意：我们只忽略输入行的其余部分。
             }
         }
 
-        // Now check whether this word is known. If so, create a command.    现在检查这个词是否已知。如果是，请创建一个命令
-        // with it. If not, create a "null" command (for unknown command).   用它。如果不是，则创建一个“null”命令（对于未知命令）。
+        // 现在检查这个词是否已知。如果是，请创建一个命令使用它。如果不是，则创建一个“null”命令（对于未知命令）。
         if(commands.isCommand(word1)) {
             return new Command(word1, word2);
         }
@@ -66,8 +61,7 @@ public class Parser
         }
     }
 
-    /**
-     * Print out a list of valid command words.     
+    /**   
      * 打印出有效命令词的列表。
      */
     public void showCommands()
--- sept-task1-wenfeng9321-master/src/cn/edu/whut/sept/zuul/Room.java
+++ sept-task1-wenfeng9321-master/src/cn/edu/whut/sept/zuul/Room.java
@@ -8,27 +8,43 @@ public class Room
     private String description;
     private HashMap<String, Room> exits;
 
+    /**
+    *定义房间和房间的出口
+    */
     public Room(String description)
     {
         this.description = description;
         exits = new HashMap<>();
     }
-
+    
+    /**
+    *输出每个房间的相邻房间信息
+    */
     public void setExit(String direction, Room neighbor)
     {
         exits.put(direction, neighbor);
     }
-
+    
+    /**
+     * @return
+     */
     public String getShortDescription()
     {
         return description;
     }
-
+    
+    /**
+    *@return 输出所在位置
+    */
     public String getLongDescription()
     {
         return "You are " + description + ".\n" + getExitString();
     }
+    
 
+    /**
+     * @return 出口方向
+     */
     private String getExitString()
     {
         String returnString = "Exits:";
@@ -38,7 +54,12 @@ public class Room
         }
         return returnString;
     }
-
+    
+    
+    /**
+     * @param direction 要走的出口方向
+     * @return 走到下一个房间
+     */
     public Room getExit(String direction)
     {
         return exits.get(direction);
